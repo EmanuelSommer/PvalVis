48020d7c1a498d07a2fb79b7d6b0d4dec48927f6
Emanuel Sommer <emanuel.sommer@web.de>
2019-06-21 13:03:26 +0200

    df

--- R/F_dist.R
+++ R/F_dist.R
@@ -40,7 +40,7 @@ F_pval<-function(F_value=5,df1=3,df2=5){
 
   new_data<-data.frame(a=max(0,(mean-5*sd)):(mean+5*sd))
   ggplot2::ggplot(new_data,aes(x=a))+
-    stat_function(fun = df,args = list(df1=df1,df2=df2))+
+    stat_function(fun = "df",args = list(df1=df1,df2=df2))+
     geom_point(aes(x=mean,y=0,col="mean"),size=4,shape=17)+
     geom_vline(xintercept = sd_bounds,linetype="dotted",
                col="#990000")+
